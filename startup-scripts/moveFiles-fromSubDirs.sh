0f0dcf4532a18409ef8d9db0fe82fcf8cdee427d
eternumarray5280 <eternumarray@gmail.com>
2026-08-29 00:45:03 -0600

    organized files - removed bogus folders

--- startup-scripts/moveFiles-fromSubDirs.sh
+++
@@ -1,2 +0,0 @@
-find . -type f -exec mv -i {} . \;
-find . -depth -mindepth 1 -type d -empty -delete
--- startup-scripts/start-rdp.sh
+++
@@ -1,8 +0,0 @@
-grdctl rdp enable
-grdctl vnc enable
-grdctl vnc set-password 4679
-grdctl vnc disable-view-only
-grdctl vnc set-auth-method password
-systemctl --user enable --now gnome-remote-desktop.service
-grdctl rdp disable-view-only
-grdctl status --show-credentials
